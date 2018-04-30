1a31a77d473fdfd08ca185527f481cb732a08e29
baptiste demurger <baptiste.demurger@insa-lyon.fr>
2018-04-30 18:03:01 +0200

    Ajout du constructeurà Environnement

--- Environnement.cpp
+++ Environnement.cpp
@@ -3,6 +3,17 @@
 #include "Environnement.h"
 
 //Constructors
+Environnement::Environnement (int W, int H, double Ainit, Case** grille; float D; int L, int S, int T){
+  W_ = W; 
+  H_ = H;
+  Ainit_ = Ainit; 
+  grille_ = grille; 
+  D_= D; 
+  L_= L; 
+  S_ = S; 
+  T_ = T;
+
+}
     
 //Destructors
 
